3c326007e6779faf86e74e1263924ec98a33677f
Anass Belcaid <anass.belcaid@gmail.com>
2022-12-10 14:17:01 +0100

    [Day 6 part1 ]

---
+++ 2022/day6/part1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
+
+using cache = std::unordered_map<char, int>;
+
+void removeKey(cache &C, char c)
+{
+  // function to remove a key from the map
+  if( C[c] > 1)
+    C[c]--;
+  else
+    C.erase(c);
+}
+
+using namespace std;
+
+
+int solve(string &line)
+{
+  int i = 4;
+  cache window;
+  //pushing the first four character
+  for(int j =0; j < 4; j++)
+    window[line[j]]++;
+  while( i < line.size())
+  {
+    if(window.size() == 4)
+      return i;
+    
+    removeKey(window, line[i-4]);
+    window[line[i++]]++;
+  }
+  return i + 1;
+
+}
+
+int main (int argc, char *argv[])
+{
+  string line;
+  getline(cin, line);
+
+  cout << solve(line) << endl;
+
+  return 0;
+}
+
