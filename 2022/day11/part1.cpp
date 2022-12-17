4cfca7963a995718a66539aa87eb60677a571229
Anass Belcaid <anass.belcaid@gmail.com>
2022-12-17 10:57:10 +0100

    [day 11 part 1]

---
+++ 2022/day11/part1.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+struct Monkey
+{
+  //represent the rule of a monkey
+  Monkey(){
+    items = list<long long>();
+  }
+  list<long long> items;   // list of holded items
+  char operation;    // my operation for update
+  string operand;       // operand to manipulate the list
+  int divideRule;    //integer to divide by
+  int indexTrue;     // index monkey if true
+  int indexFalse;    // index monkey if false;
+
+  long long computeNewValue(long long old)
+  {
+    long long val;
+    if( operand == "old")
+      val = old;
+    else
+      val = stoll(operand);
+
+    if( operation == '*')
+      return old * val;
+    if(operation == '+')
+      return old + val;
+    if(operation == '-')
+      return old - val;
+    if(operation == '/')
+      return old / val;
+    return 0;
+  }
+
+
+  void print()const
+  {
+    cout << "items ";
+    for(auto v : items)
+      cout << v << " ";
+    cout << endl;
+    cout << operation << ", " << operand << endl;
+    cout << "Divisible " << divideRule << " true " << indexTrue << ",  false " << indexFalse << endl;
+    cout << "--------------------------------" << endl;
+  }
+};
+
+class Monkeys
+{
+  // Class to represent the monkeyes
+  public:
+  vector<Monkey> monkeys;
+  vector<int> inspected;
+  Monkeys(){
+    monkeys = vector<Monkey>();
+    inspected = vector<int>();
+  }
+
+  long long monkeyBusiness()
+  {
+    sort(inspected.begin(), inspected.end());
+
+    int n = inspected.size();
+    return inspected[n-1] * inspected[n-2];
+  }
+  void playMonkey(int index)
+  {
+    // function to execute the round a monkey [index]
+    auto &M = monkeys[index];
+    while(M.items.size())
+    {
+      inspected[index]++;
+      auto old = M.items.front();
+      M.items.pop_front();
+
+      //compute the new val by the new rule
+      auto newVal = M.computeNewValue(old);
+      // cout << "val " << newVal << endl;
+
+      //reduction
+      newVal /= 3;
+
+      //now sendinging
+      if(newVal % M.divideRule == 0)
+      {
+        // cout << "Sending " << newVal << " to " << M.indexTrue << endl;
+        monkeys[M.indexTrue].items.push_back(newVal);
+       }
+      else
+      {
+        // cout << "Sending " << newVal << " to " << M.indexFalse << endl;
+        monkeys[M.indexFalse].items.push_back(newVal);
+      }
+    }
+  }
+  void round()
+  {
+    for(int i=0; i < (int)(monkeys.size()); i++)
+      playMonkey(i);
+
+  }
+  public:
+  void readMonkeyData(ifstream & in)
+  {
+    Monkey M;
+    // function to read the data of an
+    string line;
+    //line with the monkey index
+    getline(in, line);
+
+    //reading the starting items
+    getline(in, line, ':');
+    getline(in,line);
+    stringstream ss(line);
+    int value;
+    char c;
+    while(  ss >> value)
+    {
+      M.items.push_back(value);
+      ss >> c;
+    }
+    // getting the update rule
+    getline(in, line,'=');
+    getline(in, line);
+    string word;
+    ss = stringstream(line);
+    ss >> word >> M.operation >> M.operand;
+
+    //getting the divisibility
+    getline(in, line,':');
+    getline(in, line);
+    ss = stringstream(line);
+    ss >> word >> word >> M.divideRule;
+
+    //getting the two monkeys index
+    getline(in, line,':');
+    getline(in, line);
+    ss = stringstream(line);
+    ss >> word >> word >> word >> M.indexTrue;
+
+    getline(in, line,':');
+    getline(in, line);
+    ss = stringstream(line);
+    ss >> word >> word >> word >> M.indexFalse;
+
+    getline(in, line);
+
+
+    monkeys.push_back(M);
+    inspected.push_back(0);
+
+  }
+
+};
+int main (int argc, char *argv[])
+{
+  
+  // string filename = "sample";
+  string filename = "input1";
+  ifstream in(filename);
+  Monkeys game;
+  while(!in.fail())
+  {
+     game.readMonkeyData(in);
+  }
+  for(int i=0; i < 20; i++)
+    game.round();
+
+  cout << game.monkeyBusiness() << endl;
+  return 0;
+}
