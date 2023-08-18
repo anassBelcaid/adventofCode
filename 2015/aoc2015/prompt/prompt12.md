f62182df2e0027e577e24edf9929c00fd1acd515
Anass Belcaid <anass.belcaid@gmail.com>
2023-08-18 20:15:12 +0100

    [2015 day12] Parsser with json

--- 2015/aoc2015/prompt/prompt12.md
+++ 2015/aoc2015/prompt/prompt12.md
@@ -16,8 +16,24 @@ You will not encounter any strings containing numbers.
 
 What is the *sum of all numbers* in the document?
 
-To begin, [get your puzzle input](12/input).
+Your puzzle answer was `111754`.
+
+The first half of this puzzle is complete! It provides one gold star: \*
+
+\--- Part Two ---
+----------
+
+Uh oh - the Accounting-Elves have realized that they double-counted everything *red*.
+
+Ignore any object (and all of its children) which has any property with the value `"red"`. Do this only for objects (`{...}`), not arrays (`[...]`).
+
+* `[1,2,3]` still has a sum of `6`.
+* `[1,{"c":"red","b":2},3]` now has a sum of `4`, because the middle object is ignored.
+* `{"d":"red","e":[1,2,3,4],"f":5}` now has a sum of `0`, because the entire structure is ignored.
+* `[1,"red",5]` has a sum of `6`, because `"red"` in an array has no effect.
 
 Answer:
 
-You can also [Shareon [Twitter](https://twitter.com/intent/tweet?text=%22JSAbacusFramework%2Eio%22+%2D+Day+12+%2D+Advent+of+Code+2015&url=https%3A%2F%2Fadventofcode%2Ecom%2F2015%2Fday%2F12&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
+Although it hasn't changed, you can still [get your puzzle input](12/input).
+
+You can also [Shareon [Twitter](https://twitter.com/intent/tweet?text=I%27ve+completed+Part+One+of+%22JSAbacusFramework%2Eio%22+%2D+Day+12+%2D+Advent+of+Code+2015&url=https%3A%2F%2Fadventofcode%2Ecom%2F2015%2Fday%2F12&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
--- 2015/aoc2015/src/day12.rs
+++ 2015/aoc2015/src/day12.rs
@@ -1,26 +1,50 @@
-use fancy_regex::Regex;
+use serde_json::value;
+use serde_json::Map;
+use serde_json::Value;
 use std::fs::File;
 use std::io::Read;
 
-fn part1(content: &str) -> i64 {
-    // function to read the Json file and compute the sum of all the numbers
-    // we will use (not sure and I will bite my tongue in part 2) regular expressions
-    // to parse all the numbers
-
-    let pattern = Regex::new(r"-?\d+").unwrap();
-    let mut sum = 0i64;
-
-    for number in pattern.find_iter(&content) {
-        let val: i64 = number
-            .expect("invalid regular")
-            .as_str()
-            .parse()
-            .expect("Invalid number");
-
-        // println!("val is {val}");
-        sum += val;
+fn sum_numeric_values(data: &Value) -> i64 {
+    // Recursive function to compute the sum of the numeric values in the data
+    match data {
+        Value::Number(a) => a.as_i64().unwrap(),
+        Value::Array(arr) => arr.iter().map(sum_numeric_values).sum(),
+        Value::Object(map) => {
+            let mut sum = 0;
+            for (_key, val) in map {
+                sum += sum_numeric_values(val);
+            }
+            sum
+        }
+        _ => 0,
     }
-    sum
+}
+
+fn sum_numeric_values_exclude_reds(data: &Value) -> i64 {
+    // Recursive function to compute the sum of the numeric values in the data
+
+    let color = String::from("red");
+
+    // first exclude the reds
+    if let Value::Object(map) = data {
+        for (_key, value) in map {
+            if value.is_string() && value.to_string() == "\"red\"" {
+                return 0;
+            }
+        }
+    }
+    match data {
+        Value::Number(a) => a.as_i64().unwrap(),
+        Value::Array(arr) => arr.iter().map(sum_numeric_values_exclude_reds).sum(),
+        Value::Object(map) => {
+            let mut sum = 0i64;
+            for (_key, value) in map {
+                sum += sum_numeric_values_exclude_reds(value);
+            }
+            sum
+        }
+        _ => 0,
+    }
 }
 
 fn main() {
@@ -31,7 +55,14 @@ fn main() {
     file.read_to_string(&mut content)
         .expect("invalid content in the file");
 
-    // Solvign the first part
-    let answer1 = part1(&content);
-    println!("part I = {answer1}");
+    // Let begin part2
+    let parsed: Value = serde_json::from_str(&content).expect("Invalid data");
+
+    // Answer 1
+    let answer1 = sum_numeric_values(&parsed);
+    println!("Part I = {answer1}");
+
+    // Answer 2
+    let answer2 = sum_numeric_values_exclude_reds(&parsed);
+    println!("Part II = {answer2}");
 }
