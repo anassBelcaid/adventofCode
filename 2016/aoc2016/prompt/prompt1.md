ab583a33bbd0ad3746de925c0c321787bce4ca8f
anassBelcaid <anass.belcaid@gmail.com>
2023-10-08 09:33:41 +0100

    [2016] Day 1 part II

--- 2016/aoc2016/prompt/prompt1.md
+++ 2016/aoc2016/prompt/prompt1.md
@@ -21,8 +21,6 @@ For example:
 
 Your puzzle answer was `271`.
 
-The first half of this puzzle is complete! It provides one gold star: \*
-
 \--- Part Two ---
 ----------
 
@@ -32,8 +30,12 @@ For example, if your instructions are `R8, R4, R4, R8`, the first location you v
 
 How many blocks away is the *first location you visit twice*?
 
-Answer:
+Your puzzle answer was `153`.
 
-Although it hasn't changed, you can still [get your puzzle input](1/input).
+Both parts of this puzzle are complete! They provide two gold stars: \*\*
 
-You can also [Shareon [Twitter](https://twitter.com/intent/tweet?text=I%27ve+completed+Part+One+of+%22No+Time+for+a+Taxicab%22+%2D+Day+1+%2D+Advent+of+Code+2016&url=https%3A%2F%2Fadventofcode%2Ecom%2F2016%2Fday%2F1&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
+At this point, you should [return to your Advent calendar](/2016) and try another puzzle.
+
+If you still want to see it, you can [get your puzzle input](1/input).
+
+You can also [Shareon [Twitter](https://twitter.com/intent/tweet?text=I%27ve+completed+%22No+Time+for+a+Taxicab%22+%2D+Day+1+%2D+Advent+of+Code+2016&url=https%3A%2F%2Fadventofcode%2Ecom%2F2016%2Fday%2F1&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
--- 2016/aoc2016/src/day1.rs
+++ 2016/aoc2016/src/day1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 
@@ -7,6 +8,7 @@ enum Turn {
     L, // turn left
 }
 
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
 enum Direction {
     NORTH,
     EAST,
@@ -30,6 +32,7 @@ impl Instruction {
     }
 }
 
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
 struct Position {
     x: i32,
     y: i32,
@@ -48,6 +51,111 @@ impl Position {
     pub fn manhatan_distance(&self) -> i32 {
         self.x.abs() + self.y.abs()
     }
+
+    pub fn walk_with_memory(
+        &mut self,
+        instruction: &Instruction,
+        cache: &mut HashSet<(i32, i32)>,
+    ) -> bool {
+        // function to walk with memory thereturn bool is for informing the user
+        // either we did encounter a seen the position
+        // if so we can never continue
+        if instruction.turn == Turn::R {
+            match self.dir {
+                Direction::NORTH => {
+                    self.dir = Direction::EAST;
+                    // self.x += instruction.steps
+                    for _i in 0..instruction.steps {
+                        self.x += 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+                Direction::EAST => {
+                    self.dir = Direction::SOUTH;
+                    for _i in 0..instruction.steps {
+                        self.y -= 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+                Direction::SOUTH => {
+                    self.dir = Direction::OUEST;
+                    // self.x -= instruction.steps
+
+                    for _i in 0..instruction.steps {
+                        self.x -= 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+                Direction::OUEST => {
+                    self.dir = Direction::NORTH;
+                    for _i in 0..instruction.steps {
+                        self.y += 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+            }
+        } else {
+            match self.dir {
+                Direction::NORTH => {
+                    self.dir = Direction::OUEST;
+                    // self.x -= instruction.steps
+                    for _i in 0..instruction.steps {
+                        self.x -= 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+                Direction::EAST => {
+                    self.dir = Direction::NORTH;
+
+                    for _i in 0..instruction.steps {
+                        self.y += 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+                Direction::SOUTH => {
+                    self.dir = Direction::EAST;
+                    for _i in 0..instruction.steps {
+                        self.x += 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+                Direction::OUEST => {
+                    self.dir = Direction::SOUTH;
+
+                    for _i in 0..instruction.steps {
+                        self.y -= 1;
+                        if cache.contains(&(self.x, self.y)) {
+                            return true;
+                        }
+                        cache.insert((self.x, self.y));
+                    }
+                }
+            }
+        }
+        false
+    }
+
     pub fn execute(&mut self, instruction: &Instruction) {
         // function to move the current position
         if instruction.turn == Turn::R {
@@ -92,11 +200,24 @@ impl Position {
     }
 }
 
-pub fn partI(instructions: &str) -> i32 {
+pub fn part_i(instructions: &str) -> i32 {
     let mut position = Position::new(0, 0);
 
     for instruction in instructions.split(',').map(Instruction::new) {
         position.execute(&instruction);
+    }
+    position.manhatan_distance()
+}
+
+pub fn part_ii(instructions: &str) -> i32 {
+    let mut position = Position::new(0, 0);
+    let mut cache = HashSet::new();
+
+    for instruction in instructions.split(',').map(Instruction::new) {
+        let seen = position.walk_with_memory(&instruction, &mut cache);
+        if seen {
+            return position.manhatan_distance();
+        }
     }
     position.manhatan_distance()
 }
@@ -110,6 +231,9 @@ pub fn main() {
 
     // The directions are split by , so let's read the input
 
-    let answer1 = partI(&directions);
+    let answer1 = part_i(&directions);
     println!("Part I = {answer1}");
+
+    let answer2 = part_ii(&directions);
+    println!("Part II = {answer2}");
 }
