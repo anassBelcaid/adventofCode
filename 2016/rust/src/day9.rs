dca56caef64167f50b3d094ec2ed11e4be316527
anassBelcaid <anass.belcaid@gmail.com>
2023-10-22 08:38:38 +0100

    [2016]  part 2

--- 2016/rust/src/day9.rs
+++ 2016/rust/src/day9.rs
@@ -1,93 +1,145 @@
+use regex::Regex;
 use std::fs;
 #[test]
-fn test1() {
+fn test_v1_1() {
     let line = "ADVENT".to_string();
     // /no markers and decompresses to itself with no changes, resulting in a decompressed length of `6`.
-    let output = decoded_length(&line);
+    let output = len_decompressed(&line);
     assert_eq!(output, 6);
-
+}
+#[test]
+fn test_v1_2() {
     let line = "A(1x5)BC".to_string();
     // repeats only the `B` a total of `5` times, becoming `ABBBBBC` for a decompressed length of `7`.
-    let output = decoded_length(&line);
+    let output = len_decompressed(&line);
     assert_eq!(output, 7);
+}
 
+#[test]
+fn test_v1_3() {
     let line = "(3x3)XYZ".to_string();
-    let output = decoded_length(&line);
+    let output = len_decompressed(&line);
     assert_eq!(output, 9);
+}
 
+#[test]
+fn test_v1_4() {
     let line = "A(2x2)BCD(2x2)EFG".to_string();
-    let output = decoded_length(&line);
+    let output = len_decompressed(&line);
     assert_eq!(output, 11);
-
+}
+#[test]
+fn test_v1_5() {
     let line = "(6x1)(1x3)A".to_string();
-    let output = decoded_length(&line);
+    let output = len_decompressed(&line);
     assert_eq!(output, 6);
+}
 
+#[test]
+fn test_v1_6() {
     let line = "X(8x2)(3x3)ABCY".to_string();
-    let output = decoded_length(&line);
+    let output = len_decompressed(&line);
     assert_eq!(output, 18);
 }
 
-fn next_integer(line: &Vec<char>, index: &mut usize) -> i32 {
-    // function to return  to read the integer starting from the position [index]
-    // and return the next position
-    let mut value = 0;
-    assert!(line[*index].is_digit(10));
-
-    while line[*index].is_digit(10) {
-        value *= 10;
-        value += (line[*index] as u8 - '0' as u8) as i32;
-        *index += 1;
-    }
-    value
+#[test]
+fn test_v2_1() {
+    let line = "(3x3)XYZ".to_string();
+    let output = len_decompressed_v2(&line);
+    assert_eq!(output, 9);
 }
 
-fn decoded_length(line: &str) -> usize {
-    // compute the lenght of the decompressed line
-    let mut index = 0;
-    let mut count = 0usize;
-    let n = line.len();
-    let line: Vec<char> = line.chars().collect();
-
-    while index < n {
-        // normal character
-        // println!("in char {}", line[index]);
-        if line[index].is_whitespace() {
-            index += 1;
-        } else if line[index].is_alphabetic() {
-            count += 1;
-            index += 1;
-        } else {
-            // assuming a good line this will be a opening parenthesis
-            // get the
-            // skip the opening parenthesis
-            index += 1;
-            let num_char = next_integer(&line, &mut index);
-
-            // escape the x
-            index += 1;
-
-            // reading how many time to repeat
-            let repeat = next_integer(&line, &mut index);
-            // println!("repeat is {repeat}\t num_char = {num_char}");
-
-            // escaping the closing character
-            index += 1;
-
-            // now we escape the repeated character since we will add them
-            count += repeat as usize * num_char as usize;
-
-            index += num_char as usize;
-        }
-    }
-    count
+#[test]
+fn test_v2_2() {
+    let line = "X(8x2)(3x3)ABCY".to_string();
+    let output = len_decompressed_v2(&line);
+    assert_eq!(output, 20);
 }
+
+#[test]
+fn test_v2_3() {
+    let line = "(27x12)(20x12)(13x14)(7x10)(1x12)A".to_string();
+    let output = len_decompressed_v2(&line);
+    assert_eq!(output, 241920);
+}
+
+#[test]
+fn test_v2_4() {
+    let line = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN".to_string();
+    let output = len_decompressed_v2(&line);
+    assert_eq!(output, 445);
+}
+
+fn len_decompressed(region: &str) -> usize {
+    // function to compute the lenght of the
+    // region of interest (could be a slice)
+    let pattern = Regex::new(r"\((\d+)x(\d+)\)").expect("invalid regex");
+
+    if let Some(m) = pattern.find(region) {
+        // Getting the region of interest in the pattern
+        let start = m.start();
+        let end = m.end();
+        // println!("string is {region}");
+        // println!("start is {start}\t end is {end}");
+
+        // now we must get the groups
+        let captures = pattern.captures(region).unwrap();
+
+        let num_char: usize = captures[1].parse().unwrap();
+        let repeat: usize = captures[2].parse().unwrap();
+
+        return start + num_char * repeat + len_decompressed(&region[end + num_char..]);
+    } else {
+        return region.len();
+    }
+}
+
+fn len_decompressed_v2(region: &str) -> usize {
+    // function to compute the lenght of the
+    // region of interest (could be a slice)
+    let pattern = Regex::new(r"\((\d+)x(\d+)\)").expect("invalid regex");
+
+    if let Some(m) = pattern.find(region) {
+        // Getting the region of interest in the pattern
+        let start = m.start();
+        let end = m.end();
+        // println!("string is {region}");
+        // println!("start is {start}\t end is {end}");
+
+        // now we must get the groups
+        let captures = pattern.captures(region).unwrap();
+
+        let num_char: usize = captures[1].parse().unwrap();
+        let repeat: usize = captures[2].parse().unwrap();
+
+        return start
+            + len_decompressed_v2(&region[end..end + num_char]) * repeat
+            + len_decompressed_v2(&region[end + num_char..]);
+    } else {
+        return region.len();
+    }
+}
+
 pub fn part_i() -> usize {
     let line = fs::read_to_string("../input/day9").expect("invalid file or content");
+    let line = line.trim();
 
-    decoded_length(&line)
+    len_decompressed(&line)
 }
+
+pub fn part_ii() -> usize {
+    let line = fs::read_to_string("../input/day9").expect("invalid file or content");
+    let line = line.trim();
+
+    len_decompressed_v2(&line)
+}
+
 pub fn main() {
+    // part i
     let answer1 = part_i();
     println!("part I = {answer1}");
+
+    // part ii
+    let answer2 = part_ii();
+    println!("part II = {answer2}");
 }
